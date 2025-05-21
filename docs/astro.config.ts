faa9230efe866ea551e90d23e3daafbf2b090ba0
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2025-05-21 08:56:00 +0200

    docs: end

--- docs/astro.config.ts
+++ docs/astro.config.ts
@@ -6,16 +6,22 @@ import { remarkAutoDiff } from 'remark-auto-diff'
 export default defineConfig({
   integrations: [
     starlight({
-      description: '// TODO(HiDeoo) ',
+      description: 'A remark plugin providing an alternative syntax to author diff code blocks in Markdown.',
       editLink: {
         baseUrl: 'https://github.com/HiDeoo/remark-auto-diff/edit/main/docs/',
       },
-      // TODO(HiDeoo) sidebar
+      sidebar: [
+        {
+          label: 'Start Here',
+          items: ['getting-started', 'why'],
+        },
+        'demo',
+      ],
       social: [
         { href: 'https://bsky.app/profile/hideoo.dev', icon: 'blueSky', label: 'Bluesky' },
         { href: 'https://github.com/HiDeoo/remark-auto-diff', icon: 'github', label: 'GitHub' },
       ],
-      title: '// TODO(HiDeoo) ',
+      title: 'remark-auto-diff',
     }),
   ],
   markdown: {
